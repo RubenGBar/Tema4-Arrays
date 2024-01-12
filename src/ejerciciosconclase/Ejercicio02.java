198e0a9026b45ce9be2c0abd33d8cb1057eceeed
RubenGBar <ruben.garcia@iesnervion.es>
2024-01-12 14:36:33 +0100

    2 sigue sin funiconar

--- src/ejerciciosconclase/Ejercicio02.java
+++ src/ejerciciosconclase/Ejercicio02.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 public class Ejercicio02 {
 	public static void main(String[] args) {
 		int tabla[] = new int[55];
+		int primero = 0;
+		int ultimo = 0;
 		
 		for(int i = 1; i <= 10; i++) {
 			
-			for(int j = 1; j <= i; j++) {
-				Arrays.fill(tabla, j);
-			}
-			
+			Arrays.fill(tabla, primero, ultimo + 1, i);
+			primero = i + 1;
+			ultimo = i;
 		}
 		
 		System.out.println(Arrays.toString(tabla));
