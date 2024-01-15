191b4ce35c5c7b8dcd7f9c7e781198db6ae07124
RubenGBar <ruben.garcia@iesnervion.es>
2024-01-15 09:15:20 +0100

    02 terminado

--- src/ejerciciosconclase/Ejercicio02.java
+++ src/ejerciciosconclase/Ejercicio02.java
@@ -4,17 +4,30 @@ import java.util.Arrays;
 
 public class Ejercicio02 {
 	public static void main(String[] args) {
+		// Array para guardar la tabla con todos los valores
 		int tabla[] = new int[55];
-		int primero = 0;
-		int ultimo = 0;
-		
-		for(int i = 1; i <= 10; i++) {
-			
-			Arrays.fill(tabla, primero, ultimo + 1, i);
-			primero = i + 1;
-			ultimo = i;
+		// Variable para saber en que posición empezar a rellenar
+		int desde = 0;
+		// Variable para saber en que posición terminar de rellenar
+		int hasta = 1;
+
+		// Bucle for para rellenar la tabla
+		for (int i = 1; i <= 10; i++) {
+			// Relleno el Array con el algoritmo apropiado
+			Arrays.fill(tabla, desde, hasta, i);
+			/*
+			 * Asigno a la variable desde el valor máximo anterior, porque ese será la
+			 * primera posición del siguiente
+			 */
+			desde = hasta;
+			/*
+			 * Asigno a la variable hasta el valor desde más el contador porque ese es el
+			 * rango a rellenar y le sumo uno ya que es un límite excluyente
+			 */
+			hasta = desde + i + 1;
 		}
 		
+		// Muestro el Array por pantalla
 		System.out.println(Arrays.toString(tabla));
 		
 	}
