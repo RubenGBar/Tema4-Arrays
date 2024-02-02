04dd62b942e1e5d289e01d7a6f7c700949cd50ca
RubenGBar <ruben.garcia@iesnervion.es>
2024-02-02 13:31:34 +0100

    05 terminado

--- src/ejerciciosarraysbidimensionales/Ejercicio05.java
+++ src/ejerciciosarraysbidimensionales/Ejercicio05.java
@@ -5,31 +5,51 @@ import java.util.Random;
 public class Ejercicio05 {
 	public static void main(String[] args) {
 		// Array para guardar el valor devuelto por la función
-		int sumas[][] = new int[4][5];
+		int sumas[][] = rellenar();
+		// Variable para guardar la suma de las columnas
+		int columnasSumadas = 0;
+		// Variable para guardar la suma de las filas
+		int filasSumadas = 0;
+		// Variable para guardar la suma total
+		int totalSumado = 0;
 		
-		// Llamo a la función sumas y guardo el valor devuelto en sumas
-		sumas = rellenar(sumas);
-		
-		// Bucles para mostrar el Array como una tabla, el primero es para controlar las filas
+		// Bucles para sumar las filas, con el primer bucle controlo las filas
 		for(int i = 0; i < sumas.length; i++) {
+			// Inicio la variable de la suma de las filas para guardar el valor anterior
+			filasSumadas = 0;
 			// Bucle para controlar las columnas
 			for(int j = 0; j < sumas[0].length; j++) {
-				// Muestro cada elemento
-				System.out.println(sumas[i][j] + "\t");
+				// Muestro cada elemento de la tabla
+				System.out.print(sumas[i][j] + "\t");
+				// Voy sumando cada elemento de la fila
+				filasSumadas += sumas[i][j];
 			}
-			// Salto de línea cuando termino de imprimir una línea
-			System.out.println();
+			// Muestro la suma de cada fila
+			System.out.println(filasSumadas);
 		}
 		
-		/*
-		 * MOSTRAR COLUMNAS Y FILAS SUMADAS Y TOTAL
-		 * */
-		
+		// Bucles para sumar las columnas y el total, con el primer bucle controlo las columnas
+		for(int j =  0; j < sumas[0].length; j++) {
+			// Inicio la variable de la suma de las columnas para guardar el valor anterior
+			columnasSumadas = 0;
+			// Bucle para controlar las filas
+			for(int i = 0; i < sumas.length; i++) {
+				// Sumo todos los elementos de una columna
+				columnasSumadas += sumas[i][j];
+			}
+			// Guardo el valor de la suma de cada columna para el total
+			totalSumado += columnasSumadas;
+			// Muestro cada columna sumada
+			System.out.print(columnasSumadas + "\t");
+		}
+		// Muestro el total de las columnas sumadas
+		System.out.println(totalSumado);
 	}
 	
-	public static int[][] rellenar(int[][] sumas) {
+	// Clase que deuvuelve una tabla rellena con valores aleatorios
+	public static int[][] rellenar() {
 		// Array que devuelvo como valor de la función
-		int tablaRellena[][] = new int[sumas.length][sumas[0].length];
+		int tablaRellena[][] = new int[4][5];
 		// Objeto de la clase random para poder generar números pseudoaleatorios
 		Random rand = new Random();
 		
@@ -41,9 +61,7 @@ public class Ejercicio05 {
 				tablaRellena[i][j] = rand.nextInt(100, 1000);
 			}
 		}
-		
 		// Devuelvo tablaRellena como valor de la función
 		return tablaRellena;
 	}
-	
 }
