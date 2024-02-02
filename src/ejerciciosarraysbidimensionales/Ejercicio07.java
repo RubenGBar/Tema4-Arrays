ded161f1e9845ed717ad3fa389f10df7cbc81a9d
ruben <ruben@DESKTOP-QTJ799Q>
2024-02-02 17:30:06 +0100

    objeto random eliminado

--- src/ejerciciosarraysbidimensionales/Ejercicio07.java
+++ src/ejerciciosarraysbidimensionales/Ejercicio07.java
@@ -1,11 +1,7 @@
 package ejerciciosarraysbidimensionales;
 
-import java.util.Random;
-
 public class Ejercicio07 {
 	public static void main(String[] args) {
-		// Creo un objeto de la clase Random para rellenar la tabla
-		Random rand = new Random();
 		// Array al que se le hará la transposición
 		int tablaNormal[][] = {{1, 2, 3, 4},
 								{5, 6, 7, 8},
